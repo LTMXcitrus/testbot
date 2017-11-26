5d91b52d3c47e1948766ff6f20f06e8ef11b1266
LTMXcitrus <mattcitron45140@gmail.com>
2017-11-27 00:41:13 +0100

    progress

--- src/main/kotlin/com/bot/slack/testbot/story/thankyou.kt
+++ src/main/kotlin/com/bot/slack/testbot/story/thankyou.kt
@@ -1,9 +1,25 @@
 package com.bot.slack.testbot.story
 
+import fr.vsct.tock.bot.connector.slack.attachmentField
+import fr.vsct.tock.bot.connector.slack.attachmentMessage
+import fr.vsct.tock.bot.connector.slack.multiLineMessage
+import fr.vsct.tock.bot.connector.slack.withSlack
 import fr.vsct.tock.bot.definition.story
 
 val thankyou = story("thankyou") { bus ->
+
+
+
     with(bus) {
-        send("De rien")
+        val attachments = arrayOf(attachmentField("title", "value"), attachmentField("title", "value"))
+        withSlack {
+            multiLineMessage(listOf("line 1", "line 2", "line 3"))
+            attachmentMessage(
+                    *attachments,
+                    fallback = "fallback",
+                    text = "text"
+            )
+        }
+        end()
     }
 }
